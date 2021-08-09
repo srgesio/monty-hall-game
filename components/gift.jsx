61b9590ec08a5560a72b07acdf20dcd952c2e664
srgesio <gesioiajm@gmail.com>
2021-08-09 13:30:07 -0300

    gift box

---
+++ components/gift.jsx
@@ -0,0 +1,21 @@
+import styles from '../styles/Gift.module.css'
+
+export default function Gift(){
+    
+    return (
+        <div className={styles.gift}>
+            <div className={styles.giftHead}>
+                Tampa
+            </div>
+            <div className={styles.giftBody}>
+                Corpo
+            </div>
+            <div className={styles.giftVerticalTape}>
+             
+            </div>
+            <div className={styles.giftHorizontalTape}>
+            
+            </div>
+        </div>
+    )
+}
